a3ae32a736cf3020692560800bab2e6cb50e7f82
Nathan Reinke <nathanjreinke@outlook.com>
2024-05-10 23:32:46 +0930

    Initial commit

---
+++ Introduction.py
@@ -0,0 +1,15 @@
+# File: Introduction.py
+# Author: Nathan Reinke
+# Date: 10 May 2024
+#
+# A short program to satisfy the requirements of the warm-up exercise for MEC4406
+
+# Write in python that prints your name, and then counts up to your favourite number squared.
+
+name = "Nathan Reinke"
+print (name)
+
+fav_num = 17
+for i in range (0,fav_num*fav_num):
+    i = i+1
+    print (i)
